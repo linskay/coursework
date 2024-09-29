afac510ef6977ea295c7a56f7458ede1492045df
Lins <alina.cheremiskina@gmail.com>
2024-09-29 12:43:08 +0300

    save 1.1

--- src/test/java/pro/sky/homework25/service/DepartmentServiceImplTest.java
+++ src/test/java/pro/sky/homework25/service/DepartmentServiceImplTest.java
@@ -1,6 +1,5 @@
 package pro.sky.homework25.service;
 
-import net.bytebuddy.pool.TypePool;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,9 +8,9 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pro.sky.homework25.Employee;
+import pro.sky.homework25.exceptions.EmployeeNotFoundException;
 
 import java.util.Collection;
 import java.util.List;
@@ -19,8 +18,7 @@ import java.util.Random;
 import java.util.stream.Stream;
 
 import static java.util.Collections.EMPTY_LIST;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 
@@ -50,6 +48,7 @@ class DepartmentServiceImplTest {
             new Employee("ддд19", "ввв", 18000, 4),
             new Employee("ддд20", "ввв", 18000, 5)
     );
+
     @Mock
     private EmployeeServiceImpl employeeService;
 
@@ -123,7 +122,7 @@ class DepartmentServiceImplTest {
         );
     }
 
-    @DisplayName("Положительный тест на получение максимальной зп по департаменту")//////вот здесь останов-очка
+    @DisplayName("Положительный тест на получение максимальной зп по департаменту")
     @ParameterizedTest
     @MethodSource("provideDataForMax")
     void getMaxSalaryEmployeeByDepartmentTest(int departmentId, int expected) {
@@ -131,7 +130,7 @@ class DepartmentServiceImplTest {
         when(employeeService.findAll())
                 .thenReturn(employeeList);
 
-        int actual = departmentService.getSalariesSumByDepartment(departmentId);
+        int actual = departmentService.getEmployeeWithMaxSalary(departmentId);
 
         verify(employeeService, times(1)).findAll();
         assertEquals(expected, actual);
@@ -140,8 +139,56 @@ class DepartmentServiceImplTest {
     private static Stream<Arguments> provideDataForMax() {
         return Stream.of(
                 Arguments.arguments(1, 20000),
-                Arguments.arguments(100, 0),
+                Arguments.arguments(2, 16000),
                 Arguments.arguments(5, 18000)
         );
+    }
+
+    @DisplayName("Негативный тест на получение максимальной зп по департаменту")
+    @Test
+    void getMaxSalaryEmployeeByDepartmentNegativeTest() {
+
+        int requestDepartmentID = 100;
+        when(employeeService.findAll())
+                .thenReturn(employeeList);
+
+        assertThrows(EmployeeNotFoundException.class, () ->
+                departmentService.getEmployeeWithMaxSalary(requestDepartmentID));
+        verify(employeeService, times(1)).findAll();
+    }
+
+    @DisplayName("Положительный тест на получение минимальной зп по департаменту")
+    @ParameterizedTest
+    @MethodSource("provideDataForMin")
+    void getMinSalaryEmployeeByDepartmentTest(int departmentId, int expected) {
+
+        when(employeeService.findAll())
+                .thenReturn(employeeList);
+
+        int actual = departmentService.getEmployeeWithMinSalary(departmentId);
+
+        verify(employeeService, times(1)).findAll();
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> provideDataForMin() {
+        return Stream.of(
+                Arguments.arguments(1, 10000),
+                Arguments.arguments(2, 11000),
+                Arguments.arguments(5, 18000)
+        );
+    }
+
+    @DisplayName("Негативный тест на получение минимальной зп по департаменту")
+    @Test
+    void getMinSalaryEmployeeByDepartmentNegativeTest() {
+
+        int requestDepartmentID = 100;
+        when(employeeService.findAll())
+                .thenReturn(employeeList);
+
+        assertThrows(EmployeeNotFoundException.class, () ->
+                departmentService.getEmployeeWithMinSalary(requestDepartmentID));
+        verify(employeeService, times(1)).findAll();
     }
 }
